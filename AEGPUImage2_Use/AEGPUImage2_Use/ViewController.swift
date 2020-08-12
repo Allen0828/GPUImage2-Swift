f591c37badf91a70324999d60db7baa74b7b28ba
张其锋 <1124700416@qq.com>
2020-08-12 16:30:53 +0800

    add AEGPUImage2_use

---
+++ AEGPUImage2_Use/AEGPUImage2_Use/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AEGPUImage2_Use
+//
+//  Created by 锋 on 2020/8/12.
+//
+
+import UIKit
+import GPUImage
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let pictureInput = PictureInput(image: UIImage(named: "001")!)
+        
+    }
+
+    
+    
+
+}
+
